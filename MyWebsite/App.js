1e8ed1de1bd2863bb6d05f81f7280545e22e6098
Andrew Lammon <andrewlammon@andrews-mbp.attlocal.net>
2024-03-19 19:36:47 -0700

    turning in my website with re visions needed

---
+++ MyWebsite/App.js
@@ -0,0 +1,11 @@
+// function App() {
+//     return (
+//       <div>
+//         <Navbar />
+//       </div>
+//     );
+//   }
+
+// const container = document.getElementById("root");
+// const root = ReactDOM.createRoot(container);
+// root.render(<App />);
