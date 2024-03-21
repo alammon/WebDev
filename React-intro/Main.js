98e4afbeb14d60dee5f94326bf6f135f40859c27
Andrew Lammon <lammondrew@gmail.com>
2024-03-21 15:32:58 -0700

    re-turning in React Children assignment

--- React-intro/Main.js
+++ React-intro/Main.js
@@ -1,31 +1,35 @@
 const cards = [
   {
-    title: 'Me And Tule',
-    img: "images/IMG1.jpeg",
+    title: "Me And Tule",
+    img: "React-intro/images/IMG1.jpeg",
   },
   {
-    title: 'Pretty Dog',
-    img: "images/IMG2.jpeg",
+    title: "Pretty Dog",
+    img: "React-intro/images/IMG2.jpeg",
   },
   {
-    title: 'Water Dog',
-    img: "images/IMG3.jpeg",
-  }
-]
-
+    title: "Water Dog",
+    img: "React-intro/images/IMG3.jpeg",
+  },
+];
 
 function Main() {
   return (
     <main>
-      <div className="flex gap-x-12 my-12 flex-wrap gap-y-6" id="card-container">
-        {cards.map((card, index) => <Card key={index} title={card.title} image={card.img} />)}
+      <div
+        className="flex gap-x-12 my-12 flex-wrap gap-y-6"
+        id="card-container"
+      >
+        {cards.map((card, index) => (
+          <Card key={index} title={card.title} image={card.img} />
+        ))}
       </div>
       <div>
         {/* this isn't working */}
         <Content paragraph="In this paragraph I am not talking about anything in particular. this is just to fill space so my my website looks gucci." />
         <Content paragraph="Once upon a time there was a dog. her name was Tule. tule was the coolest dog ever. The end. " />
       </div>
-      <Flex classNames="bg-blue-600 p-12 text-blue-100" >
+      <Flex classNames="bg-blue-600 p-12 text-blue-100">
         <span>Child tag one</span>
         <span>Child tag two</span>
         <span>Child tag three</span>
