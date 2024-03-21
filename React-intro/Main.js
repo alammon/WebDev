7918d5feefe4deda4758370cac91ef16acfab1a8
Andrew Lammon <andrewlammon@andrews-mbp.attlocal.net>
2024-03-20 17:19:50 -0700

    blah

--- React-intro/Main.js
+++ React-intro/Main.js
@@ -13,14 +13,6 @@ const cards = [
   }
 ]
 
-const para = [
-  {
-    paragraph: 'this is a paragraph',
-  },
-  {
-    paragraph: 'oogaboogah'
-  }
-]
 
 function Main() {
   return (
@@ -29,9 +21,15 @@ function Main() {
         {cards.map((card, index) => <Card key={index} title={card.title} image={card.img} />)}
       </div>
       <div>
+        {/* this isn't working */}
         <Content paragraph="In this paragraph I am not talking about anything in particular. this is just to fill space so my my website looks gucci." />
-        <Content paragraph="Once upon a time there was a dog. her name was Tule. tule was the coolest dog ever. The end. "/>
+        <Content paragraph="Once upon a time there was a dog. her name was Tule. tule was the coolest dog ever. The end. " />
       </div>
+      <Flex classNames="bg-blue-600 p-12 text-blue-100" >
+        <span>Child tag one</span>
+        <span>Child tag two</span>
+        <span>Child tag three</span>
+      </Flex>
     </main>
   );
 }
